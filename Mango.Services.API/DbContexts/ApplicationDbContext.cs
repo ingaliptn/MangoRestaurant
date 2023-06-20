0092a77b6203503402ca63d481a891cb9eb73233
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-06-20 13:55:14 +0300

    First commit

---
+++ Mango.Services.API/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.API.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
