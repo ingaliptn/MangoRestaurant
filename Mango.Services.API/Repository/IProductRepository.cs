3bf19e4610a70e3aef13f36c20786848afa56d55
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-06-20 15:07:50 +0300

    Correct

--- Mango.Services.API/Repository/IProductRepository.cs
+++ Mango.Services.API/Repository/IProductRepository.cs
@@ -1,20 +1,14 @@
 ﻿using System;
-<<<<<<< HEAD
 using Mango.Services.API.Models;
 
-=======
->>>>>>> main
 namespace Mango.Services.API.Repository
 {
     public interface IProductRepository
     {
-<<<<<<< HEAD
         Task<IEnumerable<ProductDto>> GetProducts();
         Task<ProductDto> GetProductById(int productId);
         Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
         Task<bool> DeleteProduct(int productId);
-=======
->>>>>>> main
     }
 }
 
