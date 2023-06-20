65f6f18532baa3d2c982566d598472a60d2ec483
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-06-20 14:48:09 +0300

    Create DTO for Product Model

---
+++ Mango.Services.API/Models/Dto/ResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Mango.Services.API.Models.Dto
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+    }
+}
+
