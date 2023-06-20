c6b061b0f75ec975c676d78574d7da557b0afb43
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-06-20 14:48:32 +0300

    Create Repository and Functionality

---
+++ Mango.Services.API/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mango.Services.API.Models;
+
+namespace Mango.Services.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
+
