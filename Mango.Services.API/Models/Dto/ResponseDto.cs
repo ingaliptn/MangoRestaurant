39fa0a8c33a6986a3517715434aca6ef681404f5
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-06-20 15:06:46 +0300

    Correction

--- Mango.Services.API/Models/Dto/ResponseDto.cs
+++ Mango.Services.API/Models/Dto/ResponseDto.cs
@@ -3,16 +3,13 @@ namespace Mango.Services.API.Models.Dto
 {
     public class ResponseDto
     {
-<<<<<<< HEAD
         public bool IsSuccess { get; set; } = true;
         public object Result { get; set; }
         public string DisplayMessage { get; set; } = "";
         public List<string> ErrorMessages { get; set; }
-=======
         public ResponseDto()
         {
         }
->>>>>>> main
     }
 }
 
