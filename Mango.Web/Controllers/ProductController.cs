8a1ed910460aa817d6732f03f31f5109070abc63
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-06-29 15:07:08 +0300

    Create View for Products

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mango.Web.Models;
+using Mango.Web.Models.Dto;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        // GET: /<controller>/
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
+
