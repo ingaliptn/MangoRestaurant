a54dfa6248a9896c7282740a7ac604755eb2c9a4
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-06-29 14:38:54 +0300

    Add configuration for Http and Products in Startup

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,7 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Mango.Web;
+using Mango.Web.Services;
+using Mango.Web.Services.IServices;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>();
+SD.ProductAPIBase = builder.Configuration["ServiceUrl:ProductAPI"];
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
