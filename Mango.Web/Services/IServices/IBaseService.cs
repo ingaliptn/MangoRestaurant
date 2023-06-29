0f8f29fabaa7c14df87426ae7092ceea07db333f
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-06-29 14:21:10 +0300

    BaseService for work with API

--- Mango.Web/Services/IServices/IBaseService.cs
+++ Mango.Web/Services/IServices/IBaseService.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using Mango.Web.Models;
+using Mango.Web.Models.Dto;
+
 namespace Mango.Web.Services.IServices
 {
-    public interface IBaseService
+    public interface IBaseService : IDisposable
     {
+        ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+
     }
 }
 
--- Mango.Web/Services/IServices/IProductService.cs
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -3,7 +3,7 @@ using Mango.Web.Models;
 
 namespace Mango.Web.Services.IServices
 {
-    public interface IProductService
+    public interface IProductService : IBaseService
     {
         Task<T> GetAllProductsAsync<T>();
         Task<T> GetAllProductByIdAsync<T>(int id);
